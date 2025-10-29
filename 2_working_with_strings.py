953240ba5d01e0ff93a7ac6c82b5c5340951cc11
lg-civil <lgzuniga-urz@cps.edu>
2025-10-29 13:34:54 -0500

    Update 2_working_with_strings.py

--- 2_working_with_strings.py
+++ 2_working_with_strings.py
@@ -4,79 +4,116 @@
 # ----------------------------------------
 
 # Strings are sequences of characters enclosed in quotes (' ' or " ")
-greeting = "Hello"
-name = "World"
+# greeting = "Hello"
+# name = "World"
 
-# ----------------------------------------
-# Basic String Operations
-# ----------------------------------------
+# # ----------------------------------------
+# # Basic String Operations
+# # ----------------------------------------
 
-# 1. Concatenation: Combining strings using the + operator
-message = greeting + " " + name
-print("Concatenated String:", message)  # Output: Hello World
+# # 1. Concatenation: Combining strings using the + operator
+# message = greeting + " " + name
+# print("Concatenated String:", message)  # Output: Hello World
 
 # ----------------------------------------
 # 2. String Functions
 # ----------------------------------------
 
 phrase = "Python is FUN!"
-
+name = "LUIS"
 # Convert all characters to lowercase
 print("Lowercase:", phrase.lower())  # Output: python is fun!
+print("Lowercase:", name.lower())
 
 # Convert all characters to uppercase
 print("Uppercase:", phrase.upper())  # Output: PYTHON IS FUN!
+print("Uppercase name:", name.upper())
+print("Uppercase name:", name.capitalize())
 
 # Check if all characters are uppercase
 print("Is Uppercase?", phrase.isupper())  # Output: False
+print("Is Uppercase?", name.isupper())
 
-# Find the length of the string
-print("Length of phrase:", len(phrase))  # Output: 14
+# # Find the length of the string
+# print("Length of phrase:", len(phrase))  # Output: 14
 
-# ----------------------------------------
-# 3. Indexing and Slicing
-# ----------------------------------------
+# # ----------------------------------------
+# # 3. Indexing and Slicing
+# # ----------------------------------------
+chicago_mayor = "Johnson"
+print("First character of mayor's name:", chicago_mayor[0]) 
+print("Last character of mayor's name:", chicago_mayor[-1]) 
 
-# Indexing: Access characters by position (0-based index)
-print("First character:", phrase[0])  # Output: P
-print("Last character:", phrase[-1])  # Output: !
+# print the first letter of the mayor
 
-# Slicing: Get a range of characters (start inclusive, end exclusive)
-print("Characters 1 to 4:", phrase[1:4])  # Output: yth
+print("First four letters of mayor's name:", chicago_mayor[0:4]) 
 
-# Example combining everything:
-print("Formatted Example:", (greeting + " " + name + "!").upper())
-# Output: HELLO WORLD!
+# first number in slicing is inclusive,
+# second is exclusive
+# son
+
+print("Last 3 letters of mayor's name:", chicago_mayor[-3:])
+
+last_name  = "Zuniga"
+
+print("Last 3 characters of mayor's name:", last_name[-3:]) 
 
 
-# ----------------------------------------
-# 7. Strings: Advanced Concepts
-# ----------------------------------------
+# print out docious
+# uppercase the entire string
+# print out super
 
-# Creating Strings: use single or double quotes
-greeting1 = 'Hello'
-greeting2 = "Hi there"
 
-# Printing Strings
-print(greeting1)
-print(greeting2)
+poppins = "supercalifragilisticexpialdocious"
 
-# ----------------------------------------
-# String Methods
-# ----------------------------------------
+print("First character of poppins:", poppins[-7:]) 
+print("Uppercase the string poppins:", poppins.upper())
+print("First 5 letters of poppins", poppins[0:5])
 
-sentence = "Python is fun to learn"
+# declaration of independence lenght
 
-# .split(): Splits the string into a list of words
-words = sentence.split()
-print("Split result:", words)
+declaration_of_independence = "The unanimous Declaration of the thirteen united States of America, When in the Course of human events, it becomes necessary for one people to dissolve the political bands which have connected them with another, and to assume among the powers of the earth, the separate and equal station to which the Laws of Nature and of Nature's God entitle them, a decent respect to the opinions of mankind requires that they should declare the causes which impel them to the separation."
+print("lenght of the first paragraph of the decalaration of independence: ", len(declaration_of_independence))
+# # Indexing: Access characters by position (0-based index)
+# print("First character:", phrase[0])  # Output: P
+# print("Last character:", phrase[-1])  # Output: !
 
-# .format(): Allows inserting values into strings using {}
-name = "Marvin"
-age = 35
-intro = "My name is {} and I am {} years old.".format(name, age)
-print(intro)
+# # Slicing: Get a range of characters (start inclusive, end exclusive)
+# print("Characters 1 to 4:", phrase[1:4])  # Output: yth
 
-# You can also use f-strings (Python 3.6+)
-intro_fstring = f"My name is {name} and I am {age} years old."
-print(intro_fstring)
+# # Example combining everything:
+# print("Formatted Example:", (greeting + " " + name + "!").upper())
+# # Output: HELLO WORLD!
+
+
+# # ----------------------------------------
+# # 7. Strings: Advanced Concepts
+# # ----------------------------------------
+
+# # Creating Strings: use single or double quotes
+# greeting1 = 'Hello'
+# greeting2 = "Hi there"
+
+# # Printing Strings
+# print(greeting1)
+# print(greeting2)
+
+# # ----------------------------------------
+# # String Methods
+# # ----------------------------------------
+
+# sentence = "Python is fun to learn"
+
+# # .split(): Splits the string into a list of words
+# words = sentence.split()
+# print("Split result:", words)
+
+# # .format(): Allows inserting values into strings using {}
+# name = "Marvin"
+# age = 35
+# intro = "My name is {} and I am {} years old.".format(name, age)
+# print(intro)
+
+# # You can also use f-strings (Python 3.6+)
+# intro_fstring = f"My name is {name} and I am {age} years old."
+# print(intro_fstring)
